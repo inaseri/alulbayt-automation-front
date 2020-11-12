afc5d0ceaca20e4c257e132a7dbb5caa51bd6da5
Iman Naseri <inaseri.20@gmail.com>
2020-11-12 09:34:19 +0330

    Add create certificate mokeb.

--- src/app/pages/create-certificate/create-certificate.component.ts
+++ src/app/pages/create-certificate/create-certificate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../../services/api.service";
 
 @Component({
   selector: 'app-create-certificate',
@@ -9,9 +10,41 @@ export class CreateCertificateComponent implements OnInit {
 
   showDetail = false;
   selectedData: any;
-  constructor() { }
+  id: any;
+
+  constructor(public apiService: ApiService) { }
 
   ngOnInit(): void {
+  }
+
+  printMokeb() {
+    window.print()
+  }
+
+  getMokeb() {
+    this.apiService.getMokeb(this.id).subscribe(
+      response => {
+        for (let i = 0; i < response.length; i++) {
+          if (response[i].location == 1) {
+            response[i].locationName = 'کربلا'
+          } else if (response[i].location == 2) {
+            response[i].locationName = 'نجف'
+          } else if (response[i].location == 3) {
+            response[i].locationName = 'کاظمین'
+          } else if (response[i].location == 4) {
+            response[i].locationName = 'سامرا'
+          } else if (response[i].location == 5) {
+            response[i].locationName = 'مسیر نجف کربلا'
+          } else if (response[i].location == 6) {
+            response[i].locationName = 'مسیر کاظمین کربلا'
+          } else if (response[i].location == 7) {
+            response[i].locationName = 'مسیر بصره کربلا'
+          }
+        }
+        this.selectedData = response[0];
+        this.showDetail = true;
+      }, error => alert('موکب انتخابی یافت نشد.')
+    );
   }
 
 }
